ee9492a127261101b1f054818597f9ce8a989705
Alex Bukreyev <oleksandr.bukreyev@gmail.com>
2016-12-12 01:11:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Home Page</title>
+    </head>
+    
+    <body>
+        <div class="my-logo">
+            
+            <img src="images/earth.png" alt="Earth" class="earth">
+            <img src="images/moon.png" alt="Moon" class="moon">
+        </div>
+    </body>
+</html>
